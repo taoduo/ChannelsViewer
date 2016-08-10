f494af825dd61504a79348a7c50bf177f7ed6188
Duo Tao <taod@carleton.edu>
2016-08-09 20:14:43 -0500

    added index label

--- ChannelsViewer/ViewController.m
+++ ChannelsViewer/ViewController.m
@@ -10,24 +10,43 @@
 
 @implementation ViewController
 @synthesize imageView;
+@synthesize countLabel;
+
 NSMutableArray *fileList;
 signed int currentIndex;
+int total;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+
+- (void) updateView {
+    if (currentIndex < 0) {
+        currentIndex = 0;
+    }
+    NSURL *url = fileList[currentIndex];
+    NSImage *image = [[NSImage alloc]initWithContentsOfFile:[url path]];
+    [imageView setImage:image];
+    NSString *text = [NSString stringWithFormat:@"%d / %d", currentIndex, total];
+    [countLabel setStringValue:text];
+}
+
+- (void)loadFolder: (NSString*) path {
     NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSURL *directoryURL = [NSURL URLWithString:@"file:///Users/duotao/Desktop/images"]; // URL pointing to the directory you want to browse
+    NSURL *directoryURL = [NSURL URLWithString:path]; // URL pointing to the directory you want to browse
     NSArray *keys = [NSArray arrayWithObject:NSURLIsDirectoryKey];
     NSDirectoryEnumerator *enumerator = [fileManager
-                  enumeratorAtURL:directoryURL
-                  includingPropertiesForKeys:keys
-                  options:0
-                  errorHandler:nil];
+                                         enumeratorAtURL:directoryURL
+                                         includingPropertiesForKeys:keys
+                                         options:0
+                                         errorHandler:nil];
     fileList = [NSMutableArray array];
     for (NSURL *url in enumerator) {
-        NSLog(@"%@", [url absoluteString]);
         [fileList addObject:url];
     }
     currentIndex = -1;
+    total = (int) [fileList count] - 1;
+    [self updateView];
 }
 
 - (void)setRepresentedObject:(id)representedObject {
@@ -35,25 +54,36 @@ signed int currentIndex;
 }
 
 - (IBAction)importButtonClick:(id)sender {
-    
+    NSOpenPanel*    panel = [NSOpenPanel openPanel];
+    [panel setCanChooseDirectories:YES];
+    [panel beginWithCompletionHandler:^(NSInteger result){
+        if (result == NSFileHandlingPanelOKButton) {
+            NSURL* url = [[panel URLs] firstObject];
+            if (url.isFileURL) {
+                BOOL isDir = NO;
+                if ([[NSFileManager defaultManager] fileExistsAtPath: url.path isDirectory: &isDir]
+                    && isDir) {
+                    // Here you can be certain the url exists and is a directory
+                    [self loadFolder: [url path]];
+                }
+            }
+            
+        }
+    }];
 }
 
 - (IBAction)nextButtonClick:(id)sender {
     int count = (int) [fileList count];
     if (currentIndex < (count - 1)) {
         currentIndex++;
-        NSURL *url = fileList[currentIndex];
-        NSImage *image = [[NSImage alloc]initWithContentsOfFile:[url path]];
-        [imageView setImage:image];
+        [self updateView];
     }
 }
 
 - (IBAction)prevButtonClick:(id)sender {
     if (currentIndex > 0) {
         currentIndex--;
-        NSURL *url = fileList[currentIndex];
-        NSImage *image = [[NSImage alloc]initWithContentsOfFile:[url path]];
-        [imageView setImage:image];
+        [self updateView];
     }
 }
 @end
